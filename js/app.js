beb2362f6c6fbd8bc998d8b421eea771f1dd9937
Marco Bernal Jr <bernalmjr@Marcos-MacBook-Air.local>
2014-10-20 09:37:46 -0500

    Updates

--- js/app.js
+++ js/app.js
@@ -1,36 +1,54 @@
  _.templateSettings.interpolate = /{([\s\S]+?)}/g;
 
 
+ /*var api_key = "9pd4mhlgcqva2kfk9bd4i1vd";
+
+ var etsy_url = [
+     "https://openapi.etsy.com",
+     "v2/",
+     "listings/",
+     "active.js",
+     "?",
+     "api_key=",
+     api_key,
+     "&callback=?"
+ ].join('');
+
+ $.getJSON(etsy_url).then(function(data) {
+     console.log(data);
+ });*/
+
+
  function EtsyClient(options) {
      if (!options.api_key) {
          throw new Error("Yo dawg, I heard you like APIs. Y U NO APIKEY!?!?");
      }
-     this.etsy_url = "https://openapi.etsy.com/v2/listings/114711047";
+     this.etsy_url = "https://openapi.etsy.com/";
      this.version = options.api_version || "v2/";
      this.api_key = options.api_key;
      this.complete_api_url = this.etsy_url + this.version;
 
-     this.init;
+     //this.init;
  }
 
  EtsyClient.prototype.pullAllActiveListings = function() {
-     var model = 'listings/114711047';
+     var model = 'listings/';
      var filter = 'active';
      return $.getJSON(this.complete_api_url + model + filter + ".js?api_key=" + this.api_key + "&callback=?").then(function(data) {
          console.log(data);
      });
- }
+ };
 
  EtsyClient.prototype.getListingInfo = function(id) {
-     var model = 'listings/114711047';
+     var model = 'listings';
      return $.getJSON(this.complete_api_url + model + '/' + id + ".js?api_key=" + this.api_key + "&callback=?").then(function(data) {
          console.log(data);
      });
- }
+ };
 
-EtsyClient.prototype.getUserInfo= function() {
-    var model = 'listings/114711047';
-    var person = 'userId';
+/*EtsyClient.prototype.getUserInfo= function() {
+    var model = 'listings/';
+    var person = 'user_id';
     return $.getJson(this.complete_api_url + model + person + ".js?api_key=" + this.api_key + "&callback=?").then(function(data) {
         console.log(data);
     });
@@ -56,22 +74,6 @@ EtsyClient.prototype.putActiveListingDataOnPage = function(listingsHtml, listing
 
 
 
- var api_key = "9pd4mhlgcqva2kfk9bd4i1vd";
-
- var etsy_url = [
-     "https://openapi.etsy.com",
-     "v2/",
-     "listings/114711047",
-     "active.js",
-     "?",
-     "api_key=",
-     api_key,
-     "&callback=?"
- ].join('');
-
- $.getJSON(etsy_url).then(function(data) {
-     console.log(data);
- });
 
  EtsyClient.prototype.init = function() 
     var self = 'this';
@@ -80,9 +82,9 @@ EtsyClient.prototype.putActiveListingDataOnPage = function(listingsHtml, listing
         this.pullAllActiveListings(),
         this.getListingInfo(),
         this.getUserInfo(),
-        this.loadTemplateFile(listings),
-        this.loadTemplateFile(listing),
-        this.loadTemplateFile(user)
+        this.loadTemplateFile('listings'),
+        this.loadTemplateFile('listing'),
+        this.loadTemplateFile('user')
       ).then(function(listings, listing, user, listingsHtml, listingHtml, userHtml) {
             self.putActiveListingDataOnPage(listingshtml, listings)
             self.putListingDataOnPage(listinghtml, listing)
@@ -90,8 +92,12 @@ EtsyClient.prototype.putActiveListingDataOnPage = function(listingsHtml, listing
       })
  };
 
+ /*$.getJSON(etsy_url).then(function(data) {
+     console.log(data);*/
+
  window.onload = app;
 
  function app () {
-    var Etsy = new EtsyClient('listing_id":114711047')
+    var Etsy = new EtsyClient({api_key:"9pd4mhlgcqva2kfk9bd4i1vd"});
+    Etsy.pullAllActiveListings();
  }
